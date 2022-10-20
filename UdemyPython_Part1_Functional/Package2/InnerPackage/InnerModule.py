f0f535ba313633fafea56ce627bab539a35dfedd
priyankjecrc <32001931+priyankjecrc@users.noreply.github.com>
2022-10-20 16:10:18 +0530

    Add files via upload

---
+++ UdemyPython_Part1_Functional/Package2/InnerPackage/InnerModule.py
@@ -0,0 +1,10 @@
+def innerfunc1():
+    print('innerfunc1() inside innermodule')
+
+
+def innerfunc2():
+    print('innerfunc2() inside innermodule')
+
+
+def innerfunc3():
+    print('innerfunc3() inside innermodule')
