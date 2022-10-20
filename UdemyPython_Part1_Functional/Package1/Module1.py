1aedad2a140c360bbe16b6134a90cc5ccfd95ee4
priyankjecrc <32001931+priyankjecrc@users.noreply.github.com>
2022-10-20 16:08:07 +0530

    Add files via upload

---
+++ UdemyPython_Part1_Functional/Package1/Module1.py
@@ -0,0 +1,6 @@
+print('This is Module1')
+
+def printSomething1():
+    print('inside Module1 printSomething')
+
+
---
+++ UdemyPython_Part1_Functional/Package1/Module2.py
@@ -0,0 +1,4 @@
+print('This is Module2')
+
+def printSomething2():
+    print('inside Module2 printSomething')
